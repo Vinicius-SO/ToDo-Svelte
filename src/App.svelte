48566d5e1cc99e917a9a01f6061e36df0b8f8d0f
Vinicius-SO <vhisoares39@gmail.com>
2022-02-23 17:32:09 -0300

    Project Inicialization

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <Counter></Counter>
+  <Counter></Counter>
+</main>
+
+<style>
+ 
+</style>
